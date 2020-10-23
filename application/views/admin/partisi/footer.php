3783e2e49fd988c015cf4a41e95ba618a196395c
erlina2901 <73325371+erlina2901@users.noreply.github.com>
2020-10-23 14:46:34 +0700

    Update footer.php

--- application/views/admin/partisi/footer.php
+++ application/views/admin/partisi/footer.php
@@ -15,13 +15,13 @@
 <!-- Bootstrap 4 -->
 <script src="<?= filter_var(base_url('assets/'), FILTER_DEFAULT); ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="<?= base_url('assets/'); ?>dist/js/adminlte.min.js"></script>
+<script src="<?= filter_var(base_url('assets/'), FILTER_DEFAULT); ?>dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="<?= base_url('assets/'); ?>dist/js/demo.js"></script>
+<script src="<?= filter_var(base_url('assets/'), FILTER_DEFAULT); ?>dist/js/demo.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="<?= base_url(); ?>assets/js/sweetalert2.all.min.js"></script>
-<script src="<?= base_url(); ?>assets/js/myscript.js"></script>
+<script src="<?= filter_var(base_url(), FILTER_DEFAULT); ?>assets/js/sweetalert2.all.min.js"></script>
+<script src="<?= filter_var(base_url(), FILTER_DEFAULT); ?>assets/js/myscript.js"></script>
 
 </body>
 
